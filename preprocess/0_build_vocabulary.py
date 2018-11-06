4e95d20751beb00a578c0f5417f2a91607abf136
Farzaneh Mahdisoltani <farzaneh@bolt2.ais.sandbox>
2018-11-05 21:30:48 -0500

    Update build_vocabulary with addresses

--- preprocess/0_build_vocabulary.py
+++ preprocess/0_build_vocabulary.py
@@ -4,6 +4,16 @@ import numpy as np
 import cPickle
 import scipy.io
 
+#### CURRENT EPIC FEATURES ####
+#CAPTION_TRAIN_PATH = '../../../data/epic/video_description_train.txt'
+#CAPTION_VALID_PATH = '../../../data/epic/video_description_val.txt'
+#VOCAB_PATH = '../../../data/epic/vocabulary_epic.txt'
+
+#### CURRENT BREAKFAST FEATURES ####
+CAPTION_TRAIN_PATH = '../../../data/breakfast_current/current_s1_train_caption_list'
+CAPTION_VALID_PATH = '../../../data/breakfast_current/current_s1_val_caption_list'
+VOCAB_PATH = '../../../data/breakfast_current/vocabulary_breakfast_current.txt'
+
 if __name__ == "__main__":
 
     ixtoword = {}
@@ -12,21 +22,21 @@ if __name__ == "__main__":
     wordtoix['<eos>'] = 0
 
     vocab = set()
-    with open('../../../data/epic/video_description_train.txt', 'rb') as f:
+    with open(CAPTION_TRAIN_PATH, 'rb') as f:
         for line in f:
             words = line.strip().replace(".","").split(" ")
             for w in words:
                 vocab.add(w)
-    # with open('../data/epic/vocabulary_epic.txt', 'w') as f:
-    #    for item in vocab:
-    #         print >> f, item
+    with open(VOCAB_PATH, 'w') as f:
+       for item in vocab:
+           print >> f, item
 
 
-    with open('../../../data/epic/video_description_val.txt', 'rb') as f:
+    with open(CAPTION_VALID_PATH, 'rb') as f:
         for line in f:
             words = line.strip().replace(".","").split(" ")
             for w in words:
                 vocab.add(w)
-    with open('../../../data/epic/vocabulary_epic.txt', 'w') as f:
+    with open(VOCAB_PATH, 'w') as f:
         for item in vocab:
              print >> f, item

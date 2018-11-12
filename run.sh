e8200413325469ae92c4d1ee06ccac1ddd1e9893
fmahdisoltani <fmahdisoltani@gmail.com>
2018-11-12 13:14:51 -0500

    Add running script

---
+++ run.sh
@@ -0,0 +1,10 @@
+ipython preprocess/0_build_vocabulary.py
+ipython preprocess/1_preprocess.py
+ipython preprocess/2_obtain_pretrained_word2vec.py 
+
+ipython video_tagging/1_obtain_tags.py
+ipython video_tagging/2_training_video_tagging_model.py
+
+ipython SCN_training.py
+ipython SCN_decode.py
+ipython SCN_evaluation.py 
